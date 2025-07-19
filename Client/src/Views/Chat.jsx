8439bf595a69892cd8ce8082bdca25ef2e8a48dd
ByJelias05 <garciassierra@hotmail.com>
2025-07-18 23:30:47 -0400

    Fondo agregado

---
+++ Client/src/Views/Chat.jsx
@@ -0,0 +1,12 @@
+import Fondo from "../Images/Fondo-Chat.jpg"
+import "../Styles/Chat.css"
+
+export function Chat(){
+    return(
+        <div className="Contenedor-Chat">
+            <div className="Sombra">
+                
+            </div>
+        </div>
+    )
+}
