c326f2503eb8264c00d490062cdf084fcbd76840
ByJelias05 <garciassierra@hotmail.com>
2025-07-21 14:07:53 -0400

    Estructura Configuration v1

--- Client/src/Views/Chat.jsx
+++ Client/src/Views/Chat.jsx
@@ -1,12 +1,28 @@
+import { Configuration } from "../Components/Configuration"
 import Fondo from "../Images/Fondo-Chat.jpg"
 import "../Styles/Chat.css"
 
 export function Chat(){
     return(
         <div className="Contenedor-Chat">
-            <div className="Sombra">
-                
+
+            <div className="Fondo-Chat"></div>
+
+            <div className="Paneles-Laterales">
+
+                <div className="Configuracion">
+                    <Configuration></Configuration>
+                </div>
+
+                <div className="Conversaciones">
+
+                </div>
+
+                <div className="Mensajes">
+
+                </div>
             </div>
+
         </div>
     )
 }
